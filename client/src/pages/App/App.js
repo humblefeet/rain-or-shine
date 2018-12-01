7b6b5bb9f18c0a5902a7157378f89f9d7afb9eac
Matthieu Bourgois <matthieubourgois@Matthieus-MacBook-Pro.local>
2018-11-30 17:56:25 -0800

    fixed cors  error bypass

--- client/src/pages/App/App.js
+++ client/src/pages/App/App.js
@@ -1,80 +1,93 @@
-import React, { Component } from 'react';
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from 'react-router-dom';
-import NavBar from '../../components/Nav/Nav'
-import SignInPage from '../SignInPage/SignInPage'
-import WeatherPage from '../WeatherPage/WeatherPage'
-import RecommendationsPage from '../RecommendationsPage/RecommendationsPage'
-import DetailRecommendationPage from '../DetailRecommendationPage/DetailRecommendationPage'
-import FavoritesPage from '../FavoritesPage/FavoritesPage';
-require('dotenv').config()
-const axios = require('axios');
-const base_url = 'https://localhost:3000'
-const four_square_base_url = 'https://api.foursquare.com/v2/'
-const FOURSQUARE_CLIENT_ID= process.env.REACT_APP_FOURSQUARE_CLIENT_ID
-const FOURSQUARE_CLIENT_SECRET=process.env.REACT_APP_FOURSQUARE_CLIENT_SECRET
-// const API_KEY = `?api_key=${process.env.REACT_APP_DARK_SKY_API_KEY}`;
+import React, { Component } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import NavBar from "../../components/Nav/Nav";
+import SignInPage from "../SignInPage/SignInPage";
+import WeatherPage from "../WeatherPage/WeatherPage";
+import RecommendationsPage from "../RecommendationsPage/RecommendationsPage";
+import DetailRecommendationPage from "../DetailRecommendationPage/DetailRecommendationPage";
+import FavoritesPage from "../FavoritesPage/FavoritesPage";
+require("dotenv").config();
+const axios = require("axios");
+const WEATHER_BASE_URL = "https://api.darksky.net/forecast/";
+const four_square_base_url = "https://api.foursquare.com/v2/";
+// const FOURSQUARE_CLIENT_ID = process.env.REACT_APP_FOURSQUARE_CLIENT_ID;
+// const FOURSQUARE_CLIENT_SECRET = process.env.REACT_APP_FOURSQUARE_CLIENT_SECRET;
+const API_KEY = `?api_key=${process.env.REACT_APP_DARK_SKY_API_KEY}`;
 // var userLat;
 // var userLon;
 
 class App extends Component {
-  constructor(props){
-    super(props)
-    this.state={
+  constructor(props) {
+    super(props);
+    this.state = {
       temperature: null,
       rainChance: null,
       aqi: null,
-      userLocation: '',
+      userLocation: "",
       userLat: null,
       userLon: null,
-      summary: '',
-      data: '',
+      summary: null,
+      data: null,
       places: null
-    }
+    };
   }
 
-  getUserLocation(){
+  getUserLocation() {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position)=>{
-          this.setState({
-            userLat: parseFloat(position.coords.latitude).toPrecision(12),
-            userLon: parseFloat(position.coords.longitude).toPrecision(12)
-          })
-      })
-    } else { 
+      navigator.geolocation.getCurrentPosition(position => {
+        this.setState({
+          userLat: parseFloat(position.coords.latitude).toPrecision(12),
+          userLon: parseFloat(position.coords.longitude).toPrecision(12)
+        });
+      });
+    } else {
       alert("Geolocation is not supported by this browser.");
     }
   }
+  componentWillMount() {
+    this.getUserLocation();
+  }
+  componentDidMount() {
+    this.getUserLocation();
 
-  componentDidMount(){
-    this.getUserLocation()
+    // axios
+    //   .post(`/api/weather`, {
+    //     userLat: this.state.userLat,
+    //     userLon: this.state.userLon
+    //   })
+    //   .then(response => console.log(response));
 
-    axios.post(`/api/weather`, {
-      userLat: this.state.userLat,
-      userLon: this.state.userLon
-    })
-    .then(response => console.log(response))
+    // fetch("/api/weather")
+    //   .then(result => {
+    //     this.setState({
+    //       data: result.data
+    //     });
+    //   })
+    //   .then(console.log(this.state.data));
 
-
-    axios.get('/api/weather')
-      .then( (result) => {
-        this.setState({
-          data: result.data
-        })
-      })
-      .then(console.log(this.state.data))
-
-    axios.get(`${four_square_base_url}venues/explore?ll=40.7,-74&client_id=${FOURSQUARE_CLIENT_ID}&client_secret=${FOURSQUARE_CLIENT_SECRET}&v=20181130`)
+    fetch(
+      "https://cors-anywhere.herokuapp.com/" +
+        `https://api.darksky.net/forecast/fb953733971e96653ed3ad18c8ee4db8/${
+          this.state.userLat
+        },${this.state.userLon}`
+    )
       .then(response => response.json())
-      .then(place=>  this.setState({places: place}))
+      .then(weather => this.setState({ data: weather }));
+
+    fetch(
+      `${four_square_base_url}venues/explore?ll=40.7,-74&client_id=${
+        process.env.REACT_APP_FOURSQUARE_CLIENT_ID
+      }&client_secret=${
+        process.env.REACT_APP_FOURSQUARE_CLIENT_SECRET
+      }&v=20181130`
+    )
+      .then(response => response.json())
+      .then(place => this.setState({ places: place }));
   }
 
   render() {
-    return(
+    return (
       <Router>
         <div>
           <nav>
@@ -82,26 +95,37 @@ class App extends Component {
             <i className="material-icons">menu</i>
           </nav>
           <Switch>
-            <Route exact path='/' render={(props) =>
-              <NavBar handleGetUserLocation={this.handleGetUserLocation}/>
-            }/>
-            <Route exact path='/signin' render={(props)=> <SignInPage/>}/>
-            <Route exact path='/weather'render={(props)=>
-              <WeatherPage
-                // temperature={this.state.temperature}
-                // rainChance={this.state.rainChance}
-                // aqi={this.state.aqi}
-              />}/>  
-                {console.log(this.state.places)}
+            <Route
+              exact
+              path="/"
+              render={props => (
+                <NavBar handleGetUserLocation={this.handleGetUserLocation} />
+              )}
+            />
+            <Route exact path="/signin" render={props => <SignInPage />} />
+            <Route
+              exact
+              path="/weather"
+              render={props => <WeatherPage places={this.state.places} />}
+            />
+            {console.log(this.state.places)}
+            {console.log(this.state.data)}
 
-            <Route exact path='/recommendations' render={(props)=> <RecommendationsPage/>}/>
-            <Route exact path='/recommendations/:id'render={(props)=> <DetailRecommendationPage/>}/>
-            <Route exact path='/favorites' render={() => <FavoritesPage/>}/>
+            <Route
+              exact
+              path="/recommendations"
+              render={props => <RecommendationsPage />}
+            />
+            <Route
+              exact
+              path="/recommendations/:id"
+              render={props => <DetailRecommendationPage />}
+            />
+            <Route exact path="/favorites" render={() => <FavoritesPage />} />
           </Switch>
         </div>
       </Router>
-
-    )
+    );
   }
 }
 
