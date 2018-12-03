8fee9e3f6ec421f697500b82cbe4ff0c80fad026
Matthieu Bourgois <matthieubourgois@Matthieus-MacBook-Pro.local>
2018-12-03 14:03:00 -0800

    moved foursquare to back

--- client/src/pages/App/App.js
+++ client/src/pages/App/App.js
@@ -8,25 +8,21 @@ import WeatherPage from "../WeatherPage/WeatherPage";
 import RecommendationsPage from "../RecommendationsPage/RecommendationsPage";
 import DetailRecommendationPage from "../DetailRecommendationPage/DetailRecommendationPage";
 import FavoritesPage from "../FavoritesPage/FavoritesPage";
-import Axios from "axios";
-// import Recommendation from "../../components/Recommendation/Recommendation";
+// import Axios from "axios";
 require("dotenv").config();
-// const axios = require("axios");
-const REACT_APP_DARK_SKY_WEATHER_BASE_URL = "https://api.darksky.net/forecast/";
-const DARK_SKY_WEATHER_API_KEY = process.env.REACT_APP_DARK_SKY_WEATHER_API_KEY;
-const REACT_APP_FOUR_SQUARE_BASE_URL = "https://api.foursquare.com/v2/";
-var todayDate = new Date()
-  .toISOString()
-  .slice(0, 10)
-  .split("-")
-  .join("");
-var latitude = 47.62;
-var longitude = -122.32;
-var FOURSQUARE_FETCH = `${REACT_APP_FOUR_SQUARE_BASE_URL}venues/explore?ll=${latitude},${longitude}
-  &client_id=${process.env.REACT_APP_FOURSQUARE_CLIENT_ID}
-  &client_secret=${
-    process.env.REACT_APP_FOURSQUARE_CLIENT_SECRET
-  }&v=${todayDate}`;
+// const REACT_APP_FOUR_SQUARE_BASE_URL = "https://api.foursquare.com/v2/";
+// var todayDate = new Date()
+//   .toISOString()
+//   .slice(0, 10)
+//   .split("-")
+//   .join("");
+// var latitude = 47.62;
+// var longitude = -122.32;
+// var FOURSQUARE_FETCH = `${REACT_APP_FOUR_SQUARE_BASE_URL}venues/explore?ll=${latitude},${longitude}
+//   &client_id=${process.env.REACT_APP_FOURSQUARE_CLIENT_ID}
+//   &client_secret=${
+//     process.env.REACT_APP_FOURSQUARE_CLIENT_SECRET
+//   }&v=${todayDate}`;
 
 class App extends Component {
   constructor(props) {
@@ -50,16 +46,9 @@ class App extends Component {
       weatherIcon: null,
       coordinates: null
     };
-    this.getUserLocation = this.getUserLocation.bind(this);
+    // this.getUserLocation = this.getUserLocation.bind(this);
     this.handleVenueClick = this.handleVenueClick.bind(this);
   }
-
-  // sendGeoLocationToBack(coordinates) {
-  //   Axios.get(`api/weather,  {coordinates} )
-  //     .then(result =>
-  //       this.setState({ coordinates })
-  //   );
-  //     }
 
   handleVenueClick = venueId => {
     this.setState({
@@ -67,35 +56,35 @@ class App extends Component {
     });
   };
 
-  getUserLocation = () => {
-    var options = {
-      enableHighAccuracy: true,
-      timeout: 5000,
-      maximumAge: 0
-    };
-    function success(pos) {
-      var crd = pos.coords;
-      // this.setState({
-      latitude = crd.latitude.toFixed(2);
-      longitude = crd.longitude.toFixed(2);
-      // });
-    }
-    function error(err) {
-      console.warn(`ERROR(${err.code}): ${err.message}`);
-    }
-    navigator.geolocation.getCurrentPosition(success, error, options);
-  };
+  // getUserLocation = () => {
+  //   var options = {
+  //     enableHighAccuracy: true,
+  //     timeout: 5000,
+  //     maximumAge: 0
+  //   };
+  //   function success(pos) {
+  //     var crd = pos.coords;
+  //     this.setState({
+  //       latitude: crd.latitude.toFixed(2),
+  //       longitude: crd.longitude.toFixed(2)
+  //     });
+  //   }
+  //   function error(err) {
+  //     console.warn(`ERROR(${err.code}): ${err.message}`);
+  //   }
+  //   navigator.geolocation.getCurrentPosition(success, error, options);
+  // };
 
   componentDidMount() {
-    this.getUserLocation();
+    // this.getUserLocation();
 
-    /*--- fetch from Weather API---*/
+    /*--- fetch from Backend Weather API---*/
     fetch("/api/weather")
       .then(response => response.json())
       .then(weather => {
         console.log("this is the weather object:", weather);
         return this.setState({
-          // weatherDescription: weather.currently.summary,
+          weatherDescription: weather.currently.summary,
           temperature: Math.round(weather.currently.temperature),
           temperatureMin: Math.round(weather.daily.data[0].temperatureMin),
           temperatureMax: Math.round(weather.daily.data[0].temperatureMax),
@@ -107,17 +96,16 @@ class App extends Component {
         });
       });
 
-    /*--- fetch from FourSquare ---*/
-    //if (this.state.precipitation > 50) {query indoor venues}
-    //if (this.state.precipitation < 50 && this.state.precipitation >25 && this.state.temperature > 65){query all venues}
-    //if (this.state.precipitation < 25  || weatherDescription  === 'sunny'){query all outdoor venues}
-    fetch(FOURSQUARE_FETCH)
+    /*--- fetch from Backend FourSquare API ---*/
+
+    fetch("/api/recommendations")
       .then(response => response.json())
-      .then(venues =>
-        this.setState({
-          // venues: venues.response.groups[0].items
-        })
-      );
+      .then(venues => {
+        console.log(venues);
+        return this.setState({
+          venues: venues.response.groups[0].items
+        });
+      });
   }
 
   render() {
@@ -127,8 +115,6 @@ class App extends Component {
           <nav>
             <h3>Rain-Or-Shine</h3>
             <NavButton />
-            {/* {console.log(this.state.latitude)}
-            {console.log(this.state.longitude)} */}
             {console.log(this.state.weatherIcon)}
             {console.log(this.state.venues)}
             {console.log(this.state.allWeather)}
--- routes/api.js
+++ routes/api.js
@@ -1,22 +1,41 @@
-const weatherAPIController = require("../Controllers/weatherAPIController");
 var express = require("express");
 var router = express.Router();
 require("dotenv").config();
 const axios = require("axios");
 const DARK_SKY_BASE_URL = "https://api.darksky.net/forecast/";
 const DARK_SKY_WEATHER_API_KEY = process.env.DARK_SKY_WEATHER_API_KEY;
+const FOURSQUARE_BASE_URL = "https://api.foursquare.com/v2/venues/explore";
+const FOURSQUARE_CLIENT_ID = process.env.FOURSQUARE_CLIENT_ID;
+const FOURSQUARE_CLIENT_SECRET = process.env.FOURSQUARE_CLIENT_SECRET;
 const request = require("request");
-// var app = express();
+var todayDate = new Date()
+    .toISOString()
+    .slice(0, 10)
+    .split("-")
+    .join("");
 var latitude = 47.62;
 var longitude = -122.32;
 
 router.get("/weather", function(req, res) {
-  var url = `${DARK_SKY_BASE_URL}${DARK_SKY_WEATHER_API_KEY}/${latitude},${longitude}`;
-  console.log(url);
-  request(url, function(err, response, body) {
-    // console.log("this is what we got back:", JSON.parse(body));
-    res.json(JSON.parse(body));
-  });
+    var url = `${DARK_SKY_BASE_URL}${DARK_SKY_WEATHER_API_KEY}/${latitude},${longitude}`;
+    console.log(url);
+    request(url, function(err, response, body) {
+        res.json(JSON.parse(body));
+    });
+});
+
+router.get("/recommendations", function(req, res) {
+    //if (this.state.precipitation > 50) {query indoor venues}
+    //if (this.state.precipitation < 50 && this.state.precipitation >25 && this.state.temperature > 65){query all venues}
+    //if (this.state.precipitation < 25  || weatherDescription  === 'sunny'){var query=''}
+    var url = `${FOURSQUARE_BASE_URL}?ll=${latitude},${longitude}
+        &client_id=${process.env.FOURSQUARE_CLIENT_ID}
+        &client_secret=${process.env.FOURSQUARE_CLIENT_SECRET}
+        &v=${todayDate}`;
+    console.log(url);
+    request(url, function(err, response, body) {
+        res.json(JSON.parse(body));
+    });
 });
 
 module.exports = router;
